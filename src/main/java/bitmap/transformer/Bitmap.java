932f5e6234f9e00c6151a902d7564e64f9a689b6
Nhu Trinh <nqtrinh87@gmail.com>
2019-08-09 12:19:32 -0700

    attempting a transformation

--- src/main/java/bitmap/transformer/Bitmap.java
+++ src/main/java/bitmap/transformer/Bitmap.java
@@ -1,15 +1,38 @@
 package bitmap.transformer;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
 
-public class Bitmap {
+public class Bitmap extends GraphicsConfiguration {
+  ColorModel colorModel;
+  int width;
+  int height;
+
 
   public Bitmap(BufferedImage image) {
+    this.colorModel = image.getColorModel();
+    this.width = image.getWidth();
+    this.height = image.getHeight();
   }
 
+  /*
+  private Bitmap addWhiteBorder(Bitmap bmp, int borderSize) {
+    Bitmap bmpWithBorder = Bitmap.createBitmap(bmp.getWidth() + borderSize * 2, bmp.getHeight() + borderSize * 2, bmp.getConfig());
+    Canvas canvas = new Canvas(bmpWithBorder);
+    canvas.drawColor(Color.WHITE);
+    canvas.drawBitmap(bmp, borderSize, borderSize, null);
+    return bmpWithBorder;
+}
+   */
 
-  // Method to set new transformation
-  public static void setTransformName(String transformName) {
-    this.transformName = transformName;
+
+  private Bitmap addWhiteBorder(Bitmap bmp, int borderSize){
+    bmp.width = bmp.width + borderSize * 2;
+    bmp.height = bmp.height + borderSize * 2;
+    Canvas canvas = new Canvas(bmp);
+//    canvas.draw
+
+
   }
 }
